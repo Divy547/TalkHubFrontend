ab1f1365258f913d759f9e1632eb507fbcf5977b
Divy <divyakaskgupta.com>
2024-06-19 15:12:15 +0530

    solved errors

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 
-export const socket = io("http://localhost:3001" || "https://chatting-application-backend.vercel.app/", {
+export const socket = io("https://chatting-application-backend.vercel.app/", {
   autoConnect: false,
 })
